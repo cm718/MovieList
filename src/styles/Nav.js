ded67c21e7bc77fd3b4e87fc459483b5426e6870
Chris McLaughlin <cm718@icloud.com>
2020-04-26 01:25:10 -0500

    Created styled components

---
+++ src/styles/Nav.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0;
+  color: white;
+  background-color: #333;
+  font-size: 1.5rem;
+  padding: 0.75rem;
+`
